917a611fd0d547df50dbf23391bf3f6a79709ced
Andrew Tolvstad <me@me.me>
2016-10-18 00:19:29 -0700

    Add comments and fix styling

--- crawler/timeline.py
+++ crawler/timeline.py
@@ -1,12 +1,15 @@
 import multihead
 
+# Event list item class
 class Event:
+    # Create new event
     def __init__(self, user, event, other, time):
         self.user  = user
         self.event = event
         self.other = other
         self.time  = time
     
+    # Show class names of member variables (for debug)
     def classes(self):
         return ('%s, %s, %s, %s' %
             (
@@ -17,6 +20,7 @@ class Event:
             )
         )
     
+    # Instantiate from DynamoDB event list entry
     @classmethod
     def from_dynamo(cls, user, dynamo_dict):
         return cls(
@@ -26,18 +30,22 @@ class Event:
             int(dynamo_dict['time'])
         )
     
+# Lazy sorter wrapper class
 class Aggregator:
+    # Instantiate sorter to sort by event time
     def __init__(self):
         self.sorter = multihead.Sorter(lambda event: event.time)
     
+    # Load event list into sorter
     def load(self, events):
         self.sorter.load(events)
     
+    # Lazy sort up to N items, shrinking list to fit
     def sort(self, count):
         return filter(
             lambda x: x is not None,
-	    map(
-		lambda _: self.sorter.pop(),
-		range(count)
-	    )
-	)
+            map(
+                lambda _: self.sorter.pop(),
+                range(count)
+            )
+        )
