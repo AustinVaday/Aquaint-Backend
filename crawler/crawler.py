0a9bea92b6eb7b8513af66d43d39bb0c7635c8cb
Austin Vaday <avaday0@ivc.edu>
2016-12-16 11:55:06 -0800

    Modified crawler.py to adhere to new list needed for push notifications. Not tested yet.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,7 +4,8 @@ import json
 import pymysql
 import boto3
 from boto3.dynamodb.conditions import Key
-
+from datetime import datetime
+import calendar
 import timeline
 import sqlconf
 
@@ -13,6 +14,11 @@ SOURCE_TABLE = 'aquaint-user-eventlist'
 DEST_TABLE   = 'aquaint-newsfeed'
 
 TIMELINE_LENGTH = 60
+
+# Return unix timestamp UTC time
+def get_current_timestamp():
+    utc = datetime.utcnow()
+    return calendar.timegm(utc.utctimetuple())
 
 # Instantiate DynamoDB connection
 def dynamo_table(table_name):
@@ -61,6 +67,17 @@ def read_eventlist(db, user):
         events
     )
 
+# Fetch notificaiton-timestamp for user
+def read_eventlist_notif(db, user):
+    # Get raw data
+    response = db.get_item(Key={ 'username': user })
+    
+    # No Events handler
+    if 'Item' not in response: return 0 
+    
+    # Convert raw data to integer val 
+    return response['Item']['notificationTimestamp']
+
 # Write new newsfeed to database
 def write_timeline(table, user, timeline_jsons):
     # Fetch old records
@@ -82,6 +99,19 @@ def write_timeline(table, user, timeline_jsons):
                 'data': timeline_json
             }
         )
+
+# Write new user notification timestamp to eventlist db 
+def write_eventlist_notif(table, user, notification_timestamp):
+    # Update notification timestamp. Assumes notificationTimestamp exists
+    table.update_item(
+        Key={
+            'username': user
+        },
+        UpdateExpression = 'SET notificationTimestamp = :val',
+        ExpressionAttributeValues = {
+            ':val': notification_timestamp
+        }
+    )
 
 # Instantiate MySQL connection
 def mysql_db():
@@ -184,4 +214,15 @@ def crawl():
         # Write constructed newsfeed to database
         write_timeline(dest, user, timeline_jsons)
 
+        notification_timestamp = read_eventlist_notif(source, user) 
+        print("Notification time stamp for user %s is %d" % user % notification_timestamp) 
+
+        # Generate list of new followers for push notifications
+        new_followers = [event.other for event in read_eventlist(source, user) if event.timestamp > notification_timestamp]
+        print("new_follers are: %s" % new_followers)
+
+        # PUSH NOTIFICATIONS CODE HERE
+
+        # If we send push notification successfully, update db user with new notification timestamp
+        write_eventlist_notif(source, user, get_current_timestamp())
     print('Done')
