2d225ebfd6ea7ba07080d02d201c962a185ed799
Austin Vaday <avaday0@ivc.edu>
2017-02-18 23:24:17 -0800

    Lambda API for generating scan codes is now functional.

--- mock_api/mock_api.py
+++ mock_api/mock_api.py
@@ -1,4 +1,5 @@
-import pymysql, sqlconf, requests 
+import pymysql, sqlconf, boto3, requests 
+from io import BytesIO
 
 def sql_select(sql, query):
     cursor = sql.cursor()
@@ -250,14 +251,25 @@ def didISendFollowRequest(event, sql):
 def createScanCodeForUser(event, sql): 
     if 'target' not in event: raise RuntimeError("Please specify 'target'.")
 
-    # Call unitag API to get HTTP response for scan code
-    response = unirest.get("https://unitag-qr-code-generation.p.mashape.com/api?data=%7B%22TYPE%22%3A%22text%22%2C%22DATA%22%3A%7B%22TEXT%22%3A%22Hello+World!%22%7D%7D&setting=%7B%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22ffffff%22%2C%22GRADIENT_TYPE%22%3A%22NO_GR%22%2C%22COLOR1%22%3A%22000000%22%7D%2C%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22Simple%22%7D%2C%22E%22%3A%22M%22%2C%22BODY_TYPE%22%3A0%7D",
-        headers={
-          "X-Mashape-Key": "3AQc18gTaJmshmHWJWfKnzKtNhDEp1HcAVwjsnhOAxrcaYjCn8"
-        }
-    )
+    # Set up AWS bucket
+    s3 = boto3.resource('s3')
 
-    return response.body
+    request_headers = {
+        "X-Mashape-Key": "3AQc18gTaJmshmHWJWfKnzKtNhDEp1HcAVwjsnhOAxrcaYjCn8"
+    }
+
+    # Generate this string using https://market.mashape.com/unitag/qr-code-generation
+    request_http = "https://unitag-qr-code-generation.p.mashape.com/api?data=%7B%22TYPE%22%3A%22url%22%2C%22DATA%22%3A%7B%22URL%22%3A%22www.aquaint.us/user/" + event["target"] + "%22%7D%7D&setting=%7B%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22ffffff%22%2C%22GRADIENT_TYPE%22%3A%22NO_GR%22%2C%22COLOR1%22%3A%223f729b%22%7D%2C%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22ER_IR%22%7D%2C%22LOGO%22%3A%7B%22L_NAME%22%3A%22http%3A%2F%2Faquaint.us%2Fimages%2FAquaint-Social-Emblem.png%22%2C%22EXCAVATE%22%3Atrue%7D%2C%22E%22%3A%22M%22%2C%22BODY_TYPE%22%3A5%7D"
+
+    response = requests.get(request_http, headers=request_headers) 
+
+    if response.status_code == 200 or response.status_code == 201:
+        # Upload file to S3
+        bytesIO = BytesIO(response.content)
+        s3.meta.client.upload_fileobj(bytesIO, "aquaint-userfiles-mobilehub-146546989", "public/scancodes/" + event["target"]) 
+        return 1
+    else:
+        return -1
 
 dispatch = {
     'adduser':                  adduser,
@@ -294,14 +306,14 @@ def handler(event, context):
     
     if action not in dispatch: raise RuntimeError("Invalid action: " + action)
     delegate = dispatch[action]
-    
+
     sql = pymysql.connect(
         sqlconf.endpoint,
         sqlconf.username,
         "",
         sqlconf.dbname
     )
-    
+
     result = delegate(event, sql)
     
     sql.close()
